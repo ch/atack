5f4689a2e0f8efacaebce0b898627c8ad9222051
ch <ch@templatemonster.me>
2022-04-28 12:21:17 +0300

    added iftop to status

--- status.sh
+++ status.sh
@@ -2,5 +2,5 @@
 user=root
 for host in `cat hosts.txt`; do
     echo $host
-    ssh $user@$host "hostname; uptime; ps uax | egrep 'itarmy|db100'"
+    ssh $user@$host "hostname; uptime; ps uax | egrep 'itarmy|db100'; iftop -t -s 10"
 done
