3d7ce171a71172ea14e123e744e0184db9f2c32c
ch <ch@templatemonster.me>
2022-05-02 09:10:52 +0300

    Fixed restart cron

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Setup
-1. buy vps
+1. buy [vps SHARED 2 CPUs](SHARED CPU)
 2. add your host's ips to hosts.txt
 3. change 33.33.33.33 to you **ip** at [install.sh](https://github.com/ch/atack/blob/main/install.sh#L13) 
 4. run **setup.sh**
--- status.sh
+++ status.sh
@@ -2,5 +2,5 @@
 user=root
 for host in `cat hosts.txt`; do
     echo $host
-    ssh $user@$host "hostname; uptime; ps uax | egrep 'itarmy|db100'; iftop -t -s 10"
+    ssh $user@$host "hostname; uptime; docker stats --no-stream; iftop -t -s 10 -L 5"
 done
